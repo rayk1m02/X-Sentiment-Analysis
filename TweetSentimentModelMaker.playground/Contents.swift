05a9986c32b5ee73dd9b7ca8f63eb273e7ca426c
rayk1m02 <hkapray@gmaill.com>
2024-11-13 20:32:19 -0600

    model maker

--- TweetSentimentModelMaker.playground/Contents.swift
+++ TweetSentimentModelMaker.playground/Contents.swift
@@ -5,15 +5,21 @@ import TabularData
 let fileURL = URL(fileURLWithPath: "/Users/raymondkim/Desktop/Stuff/Career/Projects/iOS & Swift/Twittermenti/labelled-apple-reviews.csv")
 
 do {
-    
     let data = try DataFrame(contentsOfCSVFile: fileURL)
     
-    let (trainingData, testingData) = data.stratifiedSplit(on: "text", by: 0.8)
+    let (trainingData, testingData) = data.stratifiedSplit(on: "class", by: 0.8)
+    
+    let parameters = MLTextClassifier.ModelParameters(
+        validation: .split(strategy: .automatic),
+        algorithm: .transferLearning(.bertEmbedding, revision: 1),
+        language: .english
+    )
     
     let sentimentClassifier = try MLTextClassifier(
         trainingData: trainingData,
         textColumn: "text",
-        labelColumn: "class"
+        labelColumn: "class",
+        parameters: parameters
     )
     
     let trainingAccuracy = (1.0 - sentimentClassifier.trainingMetrics.classificationError) * 100
